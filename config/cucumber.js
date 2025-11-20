8374756bad130759de3fe592202d24b38b0b6f63
Pradeep Singh <pradeepsingh00791@gmail.com>
2025-11-20 18:56:12 +0530

    Update cucumber.js

--- config/cucumber.js
+++ config/cucumber.js
@@ -5,7 +5,7 @@ module.exports = {
             snippetInterface: "async-await"
         },
         paths: [
-            "src/test/**/features/api.feature"
+            "src/test/**/features/*.feature"
         ],
         publishQuiet: true,
         dryRun: false,
